b72d864f091334f6afccf4b24e0f9de6fa18bee3
romainmzn <romain.mazzini@gmail.com>
2020-12-07 19:18:31 +0100

    feat: plein de choses

---
+++ src/Entity/School.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SchoolRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SchoolRepository::class)
+ */
+class School
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $school_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $language;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_position;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getSchoolName(): ?string
+    {
+        return $this->school_name;
+    }
+
+    public function setSchoolName(string $school_name): self
+    {
+        $this->school_name = $school_name;
+
+        return $this;
+    }
+
+    public function getLanguage(): ?string
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(string $language): self
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getUserLastname(): ?string
+    {
+        return $this->user_lastname;
+    }
+
+    public function setUserLastname(string $user_lastname): self
+    {
+        $this->user_lastname = $user_lastname;
+
+        return $this;
+    }
+
+    public function getUserFirstname(): ?string
+    {
+        return $this->user_firstname;
+    }
+
+    public function setUserFirstname(string $user_firstname): self
+    {
+        $this->user_firstname = $user_firstname;
+
+        return $this;
+    }
+
+    public function getUserPosition(): ?string
+    {
+        return $this->user_position;
+    }
+
+    public function setUserPosition(string $user_position): self
+    {
+        $this->user_position = $user_position;
+
+        return $this;
+    }
+}
