9bc9b8cd31b328bf01766dcd60cb9558df731c4a
mathieu-montjarret <mathieu.montjarret@icloud.com>
2020-12-09 10:57:55 +0100

    Feat: Create classroom / Create student

---
+++ src/Controller/TeacherController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Student;
+use App\Entity\Classroom;
+use App\Form\StudentType;
+use App\Form\ClassroomType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TeacherController extends AbstractController
+{
+    /**
+     * @Route("/teacher", name="teacher")
+     */
+    public function index(): Response
+    {
+        return $this->render('teacher/index.html.twig', [
+            'controller_name' => 'TeacherController',
+        ]);
+    }
+
+    /**
+     * @Route("teacher/new_student", name="create_student")
+     */
+    public function newStudent(Request $request, EntityManagerInterface $manager): Response
+    {
+            $student = new Student;
+
+        $studentForm = $this->createForm(StudentType:: class, $student);
+
+        $studentForm->handleRequest($request);
+
+        dump($request);
+
+        if($studentForm->isSubmitted() && $studentForm->isValid())
+        {
+            $manager->persist($student);
+            $manager->flush();
+
+            $this->redirectToRoute("create_student");
+        }        
+
+        return $this->render("teacher/create_student.html.twig", [
+            'studentForm' => $studentForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("teacher/new_classroom", name="create_classroom")
+     */
+    public function newClassroom(Request $request, EntityManagerInterface $manager): Response
+    {
+            $classroom = new Classroom;
+
+        $classroomForm = $this->createForm(ClassroomType:: class, $classroom);
+
+        $classroomForm->handleRequest($request);
+
+        dump($request);
+
+        if($classroomForm->isSubmitted() && $classroomForm->isValid())
+        {
+            $manager->persist($classroom);
+            $manager->flush();
+
+            $this->redirectToRoute("create_classroom");
+        }        
+
+        return $this->render("teacher/create_classroom.html.twig", [
+            'classroomForm' => $classroomForm->createView()
+        ]);
+
+    }
+}
---
+++ src/Form/StudentType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Student;
+use App\Entity\Classroom;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StudentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('lastname')
+            ->add('firstname')
+            ->add('age')
+            ->add('description')
+            ->add('photo')
+            ->add('sport')
+            ->add('music')
+            ->add('other_hobbies')
+            ->add('classrooms', EntityType::class,[
+                'label' => 'Classroom',
+                'class' => Classroom::class,
+                'choice_label' => 'classroom_name',
+                'expanded' => true,
+                'multiple' => true
+            ])
+            // ->add('studentDuos')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}
