27ec3fa94ba5b700482367f514987385ded85f11
romainmzn <romain.mazzini@gmail.com>
2020-12-09 15:30:22 +0100

    Feat:SecurityController

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Teacher;
+use App\Form\TeacherType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/security", name="security")
+     */
+    public function index(): Response
+    {
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+
+    // /**
+    //  * @Route("/inscription-teacher", name="registration_teacher")
+    //  */
+    // public function registration(Request $request)
+    // {
+    //     $teacher = new Teacher;
+
+    //     $formRegistrationTeacher = $this->createForm(TeacherType::class, $teacher);
+
+    //     $formRegistrationTeacher->handleRequest($request);
+        
+    //     return $this->render('security/registration_teacher.html.twig');
+    // }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+}
