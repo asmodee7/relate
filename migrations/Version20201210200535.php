c2e09597cb82d28f2178e80ce77f68f0b1f87a90
mathieu-montjarret <mathieu.montjarret@icloud.com>
2020-12-10 22:59:23 +0100

    Feat: Superadmin + admincreate

--- migrations/Version20201210200535.php
+++ migrations/Version20201210200535.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201210152151 extends AbstractMigration
+final class Version20201210200535 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20201210152151 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE student CHANGE age age INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE admin (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE student CHANGE age age INT NOT NULL');
+        $this->addSql('DROP TABLE admin');
     }
 }
