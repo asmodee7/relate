4b442c1b23177a7598c2f3fd85cab19c61a1f320
gaelleog <gaelle.auge95@gmail.com>
2020-12-10 23:18:20 +0100

    maj routes

--- migrations/Version20201210151914.php
+++ migrations/Version20201210151914.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201210101737 extends AbstractMigration
+final class Version20201210151914 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,14 @@ final class Version20201210101737 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE student CHANGE age age INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE student CHANGE age age INT DEFAULT NULL, CHANGE roles roles LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
+        $this->addSql('ALTER TABLE teacher ADD roles LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE student CHANGE age age INT NOT NULL');
+        $this->addSql('ALTER TABLE student CHANGE age age INT NOT NULL, CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_bin`');
+        $this->addSql('ALTER TABLE teacher DROP roles');
     }
 }
