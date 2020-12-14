70086487e7e40149700882e2d9bd7a45ccc404f8
mathieu-montjarret <mathieu.montjarret@icloud.com>
2020-12-14 21:34:22 +0100

    Feat: Frontend for all existing pages

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\AdminRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass=AdminRepository::class)
@@ -31,7 +33,7 @@ class Admin implements UserInterface
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\Length(                       
-     *        min="8",
+     *        min="4",
      *        minMessage="Votre mot de passe doit faire minimum 8 caractères !"
      * )
      * @Assert\EqualTo(
